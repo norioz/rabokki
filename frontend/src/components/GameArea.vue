2f72f4af43045554875236cad4bd54b6adff6e0f
Michael <hrtwell@gmail.com>
2021-11-20 23:51:36 -0500

    Added a way to track "moves" on a d3 "board".

--- frontend/src/components/GameArea.vue
+++ frontend/src/components/GameArea.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="GameArea">
-    <div style="flex:1;">{{PlayHistory}}</div>
+    <!-- <div style="flex:1;">{{PlayHistory}}</div> -->
+    <div id="board" style="flex:1"></div>
     <PlayerInput @theyEntered="this.onEnter" />
-  </div>
+   </div>
 </template>
 
 <script>
 import PlayerInput from "./PlayerInput";
+import * as d3 from "d3";
 export default {
   name: 'GameArea',
   components: {
@@ -14,15 +16,48 @@ export default {
   },
   data: () => {
     return {
-      PlayHistory: "foo"
+      PlayHistory: "foo",
+      svg: null,
+      EnteredText: null,
     }
   },
+  mounted() {
+    this.renderBoard();
+  },
+  
   methods: {
-    onEnter: function (theirEntry){
+    onEnter(theirEntry){
       this.PlayHistory = this.PlayHistory.concat(' ', theirEntry);
+      this.renderBoard();
+    },
+    renderBoard() {
+      const w = 1000;
+      const h = 1000;
+
+      if (this.svg === null){
+        this.svg = d3
+          .select("#board")
+          .append("svg")
+          .attr("width", w)
+          .attr("height", h);
+      
+        this.svg.append("rect")
+          .attr("stroke", "black")
+          .attr("fill", "#0000FF")
+          .attr("width", w)
+          .attr("height", h);
+      }     
+    
+      this.svg.selectAll("text").remove()
+      
+      this.svg.append("text")
+        .attr("x", w / 2)
+        .attr("y", h / 2)
+        .attr("dy", "3.5em")
+        .text(this.PlayHistory);
     }
-  }
-}
+  } 
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
